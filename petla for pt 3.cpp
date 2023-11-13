ee3c08311199e29a8318ea6a0a63581932dbc62c
Sebastian Rymarski (Uczeń) <srymarski584@technikumkreatywne.pl>
2023-11-13 13:16:28 +0100

    oppa

---
+++ petla for pt 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+int wysokosc;
+int szerokosc;
+cout << "podaj wysokosc ";
+cin >> wysokosc;
+cout << "podaj szerokosc ";
+cin >> szerokosc;
+
+
+
+
+	for(int i=0; i < 3; i++){
+		for(int j=0; j < 5; j++){
+			cout << "*";			
+		}
+		cout << endl;
+	}
+	
+}
